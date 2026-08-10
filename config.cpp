9d7d9950cb5870b6c98c0de4ade38436a5fb82fa
jxmot <webdev@stupiddog.net>
2026-08-09 23:35:19 -0500

    28 - fixes

--- config.cpp
+++ config.cpp
@@ -4,5 +4,5 @@ const char *ssid = "YOUR_SSID";
 const char *password = "YOUR_SSID_PASSWORD";
 
 // LED hardware configuration
-const uint16_t PixelCount = 4;
-const uint8_t PixelPin = 16;
+extern const uint16_t PixelCount = 4;
+extern const uint8_t PixelPin = 16;
